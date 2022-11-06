34c7a09e1a66a696093a508fff8296c5c87e377e
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-11-07 00:05:10 +0300

    copy grafana config during install

--- install.sh
+++ install.sh
@@ -59,5 +59,7 @@ wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
 sudo apt-get install grafana
+sudo wget -q https://raw.githubusercontent.com/josephgodwinkimani/systems-monitor/main/grafana.ini -O /etc/grafana/grafana.ini  
+sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/grafana-server
 sudo systemctl start grafana-server 
 sudo systemctl enable grafana-server.service
